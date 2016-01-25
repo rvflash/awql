2c0a95bd7f7d988336b4c3aa8ee8392ba8729269
hgouchet <herve.gouchet@twenga.com>
2016-01-26 00:20:46 +0100

    Add check to disable loading of environment if too slow

--- makefile.sh
+++ makefile.sh
@@ -11,6 +11,8 @@ AWQL_SHELL=$(if [ ! -z "$ZSH_NAME" ]; then echo "zsh"; else echo "bash"; fi)
 
 source "${SCRIPT_ROOT}/conf/awql.sh"
 source "${AWQL_INC_DIR}/common.sh"
+source "${AWQL_BASH_PACKAGES_DIR}/term.sh"
+source "${AWQL_BASH_PACKAGES_DIR}/time.sh"
 
 # Bashrc file path (manage Linux & Unix)
 if [[ "$AWQL_SHELL" == "bash" ]]; then
@@ -27,7 +29,12 @@ fi
 if [[ -z "$(grep "alias awql" ${BASHRC_FILE})" ]]; then
     echo >> "${BASHRC_FILE}"
     echo "# Added by AWQL makefile" >> "${BASHRC_FILE}"
-    echo "alias awql='${SCRIPT_ROOT}/awql.sh'" >> "${BASHRC_FILE}"
+    if [[ 1 -eq "$(userTimeTodoExceeded "${AWQL_AUTH_DIR}/custom/auth.sh" "0.100")" ]]; then
+        # Slow machine, do not load environment
+        echo "alias awql='env -i bash ${SCRIPT_ROOT}/awql.sh'" >> "${BASHRC_FILE}"
+    else
+        echo "alias awql='${SCRIPT_ROOT}/awql.sh'" >> "${BASHRC_FILE}"
+    fi
     source ${BASHRC_FILE}
     STATUS=$?
 else
@@ -44,16 +51,20 @@ else
 fi
 printAndExitOnError "$STATUS" "Create history file for AWQL queries"
 
+# Start creation of authentification file
+DEVELOPER_TOKEN="$(dialog "Your Google developer token" 1 "$AWQL_PROMPT_REQUIRED")"
+
 # Use Google auth or custom webservice to refresh Token
-DEVELOPER_TOKEN="$(dialog "Your Google developer token")"
-if confirm "Use Google to get access tokens"; then
-    CLIENT_ID="$(dialog "Your Google client ID")"
-    CLIENT_SECRET="$(dialog "Your Google client secret")"
-    REFRESH_TOKEN="$(dialog "Your Google refresh token")"
+if confirm "Use Google to get access tokens" "$AWQL_CONFIRM"; then
+    # Google Auth as provider
+    CLIENT_ID="$(dialog "Your Google client ID" 1 "$AWQL_PROMPT_REQUIRED")"
+    CLIENT_SECRET="$(dialog "Your Google client secret" 1 "$AWQL_PROMPT_REQUIRED")"
+    REFRESH_TOKEN="$(dialog "Your Google refresh token" 1 "$AWQL_PROMPT_REQUIRED")"
     ${AWQL_AUTH_INIT_FILE} -a "$AUTH_GOOGLE_TYPE" -c "$CLIENT_ID" -s "$CLIENT_SECRET" -r "$REFRESH_TOKEN" -d "$DEVELOPER_TOKEN"
     printAndExitOnError "$?" "Use Google as token provider"
 else
-    URL="$(dialog "Url of the web service to use to retrieve a Google access token")"
+    # Custom webservice as token provider
+    URL="$(dialog "Url of the web service to use to retrieve a Google access token" 1 "$AWQL_PROMPT_REQUIRED")"
     ${AWQL_AUTH_INIT_FILE} -a "$AUTH_CUSTOM_TYPE" -u "$URL" -d "$DEVELOPER_TOKEN"
     printAndExitOnError "$?" "Use a custom web service as token provider"
 fi
