3ca29d9e4b5e51a218d5446f01fea07f12afe607
hgouchet <herve.gouchet@twenga.com>
2016-03-27 22:27:23 +0200

    Improve query & statement create. Add unit tests

--- tests/query_createTest.sh
+++ tests/query_createTest.sh
@@ -9,21 +9,24 @@ declare -r TEST_QUERY_API_VERSION="v201601"
 declare -r TEST_QUERY_BAD_API_VERSION="v0883"
 declare -r TEST_QUERY_INVALID_METHOD="UPDATE RV_REPORT SET R='v'"
 # > Create
-declare -r TEST_QUERY_BASIC_CREATE="CREATE VIEW CAMPAIGN_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
-declare -r TEST_QUERY_BASIC_CREATE_REQUEST='([VIEW]="CAMPAIGN_REPORT" [QUERY]="CREATE VIEW CAMPAIGN_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC" [STATEMENT]="CREATE VIEW" [METHOD]="create" [REPLACE]="0" [FIELD_NAMES]="CampaignId CampaignName CampaignStatus Impressions Clicks" [DEFINITION]="([VIEW]=\"0\" [QUERY]=\"SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC\" [STATEMENT]=\"SELECT\" [WHERE]=\"Impressions > O\" [METHOD]=\"select\" [ORDER]=\"Clicks DESC\" [SORT_ORDER]=\"n 5 1\" [TABLE]=\"CAMPAIGN_PERFORMANCE_REPORT\" [FIELD_NAMES]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" [AWQL_QUERY]=\"SELECT CampaignId,CampaignName,CampaignStatus,Impressions,Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O\" [FIELDS]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" )" )'
-declare -r TEST_QUERY_REPLACE_CREATE="CREATE OR REPLACE VIEW CAMPAIGN_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
-declare -r TEST_QUERY_REPLACE_CREATE_REQUEST='([VIEW]="CAMPAIGN_REPORT" [QUERY]="CREATE OR REPLACE VIEW CAMPAIGN_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC" [STATEMENT]="CREATE OR REPLACE VIEW" [METHOD]="create" [REPLACE]="1" [FIELD_NAMES]="CampaignId CampaignName CampaignStatus Impressions Clicks" [DEFINITION]="([VIEW]=\"0\" [QUERY]=\"SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC\" [STATEMENT]=\"SELECT\" [WHERE]=\"Impressions > O\" [METHOD]=\"select\" [ORDER]=\"Clicks DESC\" [SORT_ORDER]=\"n 5 1\" [TABLE]=\"CAMPAIGN_PERFORMANCE_REPORT\" [FIELD_NAMES]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" [AWQL_QUERY]=\"SELECT CampaignId,CampaignName,CampaignStatus,Impressions,Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O\" [FIELDS]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" )" )'
-declare -r TEST_QUERY_FULL_CREATE="CREATE OR REPLACE VIEW CAMPAIGN_REPORT (Id, Name, Status, Impressions, Clicks) AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
-declare -r TEST_QUERY_FULL_CREATE_REQUEST='([VIEW]="CAMPAIGN_REPORT" [QUERY]="CREATE OR REPLACE VIEW CAMPAIGN_REPORT (Id, Name, Status, Impressions, Clicks) AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC" [STATEMENT]="CREATE OR REPLACE VIEW" [METHOD]="create" [REPLACE]="1" [FIELD_NAMES]="Id Name Status Impressions Clicks" [DEFINITION]="([VIEW]=\"0\" [QUERY]=\"SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC\" [STATEMENT]=\"SELECT\" [WHERE]=\"Impressions > O\" [METHOD]=\"select\" [ORDER]=\"Clicks DESC\" [SORT_ORDER]=\"n 5 1\" [TABLE]=\"CAMPAIGN_PERFORMANCE_REPORT\" [FIELD_NAMES]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" [AWQL_QUERY]=\"SELECT CampaignId,CampaignName,CampaignStatus,Impressions,Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O\" [FIELDS]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" )" )'
-declare -r TEST_QUERY_CREATE_COLUMN_NOT_MATCH="CREATE VIEW CAMPAIGN_REPORT (Id,Name,Status) AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
+declare -r TEST_QUERY_BASIC_CREATE="CREATE VIEW RV_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
+declare -r TEST_QUERY_BASIC_CREATE_REQUEST='([VIEW]="RV_REPORT" [QUERY]="CREATE VIEW RV_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC" [STATEMENT]="CREATE VIEW" [METHOD]="create" [REPLACE]="0" [FIELD_NAMES]="CampaignId CampaignName CampaignStatus Impressions Clicks" [DEFINITION]="([VIEW]=\"0\" [QUERY]=\"SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC\" [STATEMENT]=\"SELECT\" [WHERE]=\"Impressions > O\" [METHOD]=\"select\" [ORDER]=\"Clicks DESC\" [SORT_ORDER]=\"n 5 1\" [TABLE]=\"CAMPAIGN_PERFORMANCE_REPORT\" [FIELD_NAMES]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" [AWQL_QUERY]=\"SELECT CampaignId,CampaignName,CampaignStatus,Impressions,Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O\" [FIELDS]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" )" )'
+declare -r TEST_QUERY_REPLACE_CREATE="CREATE OR REPLACE VIEW RV_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
+declare -r TEST_QUERY_REPLACE_CREATE_REQUEST='([VIEW]="RV_REPORT" [QUERY]="CREATE OR REPLACE VIEW RV_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC" [STATEMENT]="CREATE OR REPLACE VIEW" [METHOD]="create" [REPLACE]="1" [FIELD_NAMES]="CampaignId CampaignName CampaignStatus Impressions Clicks" [DEFINITION]="([VIEW]=\"0\" [QUERY]=\"SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC\" [STATEMENT]=\"SELECT\" [WHERE]=\"Impressions > O\" [METHOD]=\"select\" [ORDER]=\"Clicks DESC\" [SORT_ORDER]=\"n 5 1\" [TABLE]=\"CAMPAIGN_PERFORMANCE_REPORT\" [FIELD_NAMES]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" [AWQL_QUERY]=\"SELECT CampaignId,CampaignName,CampaignStatus,Impressions,Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O\" [FIELDS]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" )" )'
+declare -r TEST_QUERY_FULL_CREATE="CREATE OR REPLACE VIEW RV_REPORT (Id, Name, Status, Impressions, Clicks) AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
+declare -r TEST_QUERY_FULL_CREATE_REQUEST='([VIEW]="RV_REPORT" [QUERY]="CREATE OR REPLACE VIEW RV_REPORT (Id, Name, Status, Impressions, Clicks) AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC" [STATEMENT]="CREATE OR REPLACE VIEW" [METHOD]="create" [REPLACE]="1" [FIELD_NAMES]="Id Name Status Impressions Clicks" [DEFINITION]="([VIEW]=\"0\" [QUERY]=\"SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC\" [STATEMENT]=\"SELECT\" [WHERE]=\"Impressions > O\" [METHOD]=\"select\" [ORDER]=\"Clicks DESC\" [SORT_ORDER]=\"n 5 1\" [TABLE]=\"CAMPAIGN_PERFORMANCE_REPORT\" [FIELD_NAMES]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" [AWQL_QUERY]=\"SELECT CampaignId,CampaignName,CampaignStatus,Impressions,Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O\" [FIELDS]=\"CampaignId CampaignName CampaignStatus Impressions Clicks\" )" )'
+declare -r TEST_QUERY_CREATE_COLUMN_NOT_MATCH="CREATE VIEW RV_REPORT (Id,Name,Status) AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
 declare -r TEST_QUERY_CREATE_NO_VIEW_NAME="CREATE VIEW (Id,Name,Status) AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
-declare -r TEST_QUERY_CREATE_NO_SELECT="CREATE VIEW CAMPAIGN_REPORT AS"
-declare -r TEST_QUERY_CREATE_NO_AS="CREATE VIEW CAMPAIGN_REPORT (Id,Name,Status) SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
-declare -r TEST_QUERY_CREATE_BAD_QUERY="CREATE VIEW CAMPAIGN_REPORT (Id,Name,Status) AS SELECT * FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
+declare -r TEST_QUERY_CREATE_NO_SELECT="CREATE VIEW RV_REPORT AS"
+declare -r TEST_QUERY_CREATE_NO_AS="CREATE VIEW RV_REPORT (Id,Name,Status) SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
+declare -r TEST_QUERY_CREATE_BAD_QUERY="CREATE VIEW RV_REPORT (Id,Name,Status) AS SELECT * FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
 declare -r TEST_QUERY_CREATE_INCOMPLETE_QUERY="CREATE"
+declare -r TEST_QUERY_REDUNDANT_VIEW_CREATE="CREATE VIEW CAMPAIGN_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
+declare -r TEST_QUERY_TABLE_EXISTS_CREATE="CREATE VIEW CAMPAIGN_PERFORMANCE_REPORT AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
+declare -r TEST_QUERY_RESERVED_KEYWORD_CREATE="CREATE VIEW WHERE AS SELECT CampaignId, CampaignName, CampaignStatus, Impressions, Clicks FROM CAMPAIGN_PERFORMANCE_REPORT WHERE Impressions > O ORDER BY Clicks DESC"
 
 
-readonly TEST_AWQL_CREATE="-11-11-11-01-21-01-01-21-21-21-21-21-21"
+readonly TEST_AWQL_CREATE="-11-11-11-01-21-01-01-21-21-21-21-21-21-21-21-21"
 
 function test_awqlCreateQuery ()
 {
@@ -91,6 +94,21 @@ function test_awqlCreateQuery ()
 
     #13 Check with incomplete query
     test=$(awqlCreateQuery "${TEST_QUERY_CREATE_INCOMPLETE_QUERY}" "${TEST_QUERY_API_VERSION}")
+    echo -n "-$?"
+    [[ "$test" == "${AWQL_QUERY_ERROR_VIEW}" ]] && echo -n 1
+
+    #14 Check with existing view name
+    test=$(awqlCreateQuery "${TEST_QUERY_REDUNDANT_VIEW_CREATE}" "${TEST_QUERY_API_VERSION}")
+    echo -n "-$?"
+    [[ "$test" == "${AWQL_QUERY_ERROR_VIEW_ALREADY_EXISTS}" ]] && echo -n 1
+
+    #15 Check with existing table name
+    test=$(awqlCreateQuery "${TEST_QUERY_TABLE_EXISTS_CREATE}" "${TEST_QUERY_API_VERSION}")
+    echo -n "-$?"
+    [[ "$test" == "${AWQL_QUERY_ERROR_TABLE}" ]] && echo -n 1
+
+    #16 Check with reserved keyword for view name
+    test=$(awqlCreateQuery "${TEST_QUERY_RESERVED_KEYWORD_CREATE}" "${TEST_QUERY_API_VERSION}")
     echo -n "-$?"
     [[ "$test" == "${AWQL_QUERY_ERROR_VIEW}" ]] && echo -n 1
 
