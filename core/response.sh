b8ce152106554395ca52261baadac34890c4c5c3
hgouchet <herve.gouchet@twenga.com>
2016-05-22 00:50:51 +0200

    Add mangement of aggregates, changes limit and sort methods

--- core/response.sh
+++ core/response.sh
@@ -7,65 +7,185 @@ if [[ -z "${AWQL_ROOT_DIR}" ]]; then
     source "${AWQL_CUR_DIR}/../conf/awql.sh"
 fi
 
-
 ##
-# Build a new AWQL file with only required data (between limits and with sort order requested)
-# Only accepts file with .awql as extension
-# @param string $1 File path
-# @param string $2 Limit
-# @param string $3 OrderBy
-# @return string File path
-# @returnStatus 1 If file is empty or do not exist
-# @returnStatus 2 If file exists but it is empty
-function __buildDataFile ()
+# Manage aggregate methods (distinct, count, sum, min, max, avg and group by)
+# @example ([DISTINCT]="1" [COUNT]="2 3" [SUM]="4")
+# @example ([COUNT]="2" [GROUP_BY]="1")
+# @example ([COUNT]="1")
+# @param string $1 Filepath
+# @param string $2 Aggregates
+# @param string $3 Group
+# @return string Filepath
+function __aggregateRows ()
 {
     local file="$1"
     if [[ -z "$file" || ! -f "$file" || "$file" != *"${AWQL_FILE_EXT}" ]]; then
         return 1
     fi
-    declare -i fileSize=$(wc -l < "$file")
-    if [[ ${fileSize} -le 1 ]]; then
-        # No result line in file
-        return 2
+    if [[ "$2" != "("*")" ]]; then
+        return 1
+    fi
+    declare -A aggregates="$2"
+    if [[ 0 -eq "${#aggregates[@]}" ]]; then
+        echo "$file"
+        return 0
+    fi
+    local groupBy="$3"
+
+    local extendedFile=""
+    declare -a aggregateOptions=()
+    if [[ -n "$groupBy" ]]; then
+        extendedFile+="${AWQL_AGGREGATE_GROUP}${groupBy/ /-}"
+        aggregateOptions+="-v groupByColumns=\"$groupBy\""
+    fi
+
+    local type="" fields=""
+    for type in "${!aggregates[@]}"; do
+        fields="${aggregates["${type}"]}"
+        if [[ -z "$fields" ]]; then
+            return 1
+        fi
+        case "$type" in
+            "${AWQL_AGGREGATE_AVG}")
+                aggregateOptions+="-v avgColumns=\"$fields\""
+                ;;
+            "${AWQL_AGGREGATE_DISTINCT}")
+                aggregateOptions+="-v distinctColumns=\"$fields\""
+                ;;
+            "${AWQL_AGGREGATE_COUNT}")
+                aggregateOptions+="-v countColumns=\"$fields\""
+                ;;
+            "${AWQL_AGGREGATE_MAX}")
+                aggregateOptions+="-v maxColumns=\"$fields\""
+                ;;
+            "${AWQL_AGGREGATE_MIN}")
+                aggregateOptions+="-v minColumns=\"$fields\""
+                ;;
+            "${AWQL_AGGREGATE_SUM}")
+                aggregateOptions+="-v sumColumns=\"$fields\""
+                ;;
+            *)
+                return 1
+                ;;
+        esac
+        extendedFile+="${type}${fields/ /-}"
+    done
+
+    local wrkFile="${file//${AWQL_FILE_EXT}/___${extendedFile}${AWQL_FILE_EXT}}"
+    if [[ -f "$wrkFile" ]]; then
+        # Job already done
+        echo "$wrkFile"
+        return 0
+    fi
+
+    awk -v ${aggregateOptions[@]} -f "${AWQL_TERM_TABLES_DIR}/aggregate.awk" "$file" > "$wrkFile"
+    if [[ $? -ne 0 ]]; then
+        return 1
+    fi
+
+    echo "$wrkFile"
+}
+
+##
+# Manage limit clause
+# @example 5 10
+# @param string $1 Filepath
+# @param string $2 Limits
+# @return string Filepath
+function __limitRows ()
+{
+    local file="$1"
+    if [[ -z "$file" || ! -f "$file" || "$file" != *"${AWQL_FILE_EXT}" ]]; then
+        return 1
     fi
     declare -a limit="($2)"
-    declare -a orderBy="($3)"
-
-    # Manage limit clause
-    declare -i limitRange=${#limit[@]}
-    if [[ ${limitRange} -gt 0 && ${limitRange} -le 2 ]]; then
-        # Limit size of data to display
-        local limits="${limit[@]}"
-        local wrkLimitFile="${file//${AWQL_FILE_EXT}/_${limits/ /-}${AWQL_FILE_EXT}}"
-
-        # Keep only first line for column names and lines in bounces
-        if [[ ! -f "$wrkLimitFile" ]]; then
-            if [[ ${limitRange} -eq 2 ]]; then
-                limits="$((${limit[0]}+1)),$((${limit[0]}+${limit[1]}))"
-                sed -n -e 1p -e "${limits}p" "$file" > "$wrkLimitFile"
-            else
-                limits="1,$((${limit[0]}+1))"
-                sed -n -e "${limits}p" "$file" > "$wrkLimitFile"
-            fi
-        fi
-        file="$wrkLimitFile"
+    declare -i limitRange="${#limit[@]}"
+    if [[ 0 -eq ${limitRange} ]]; then
+        echo "$file"
+        return 0
+    elif [[ ${limitRange} -ne 1 || ${limitRange} -ne 2 ]]; then
+        return 1
     fi
 
-    # Manage order clause
-    if [[ ${#orderBy[@]} -eq 3 ]]; then
-        local wrkOrderFile="${file//${AWQL_FILE_EXT}/_k${orderBy[1]}-${orderBy[2]}${AWQL_FILE_EXT}}"
-        if [[ ! -f "$wrkOrderFile" ]]; then
-            local sortOptions="-t, -k+${orderBy[1]} -${orderBy[0]}"
-            if [[ ${orderBy[2]} -eq ${AWQL_SORT_ORDER_DESC} ]]; then
-                sortOptions+=" -r"
-            fi
-            head -1 "$file" > "$wrkOrderFile"
-            sed 1d "$file" | sort ${sortOptions} >> "$wrkOrderFile"
-        fi
-        file="$wrkOrderFile"
+    # Limit size of data to display
+    local limits="${limit[@]}"
+    local wrkFile="${file//${AWQL_FILE_EXT}/_${limits/ /-}${AWQL_FILE_EXT}}"
+    if [[ -f "$wrkFile" ]]; then
+        # Job already done
+        echo "$wrkFile"
+        return 0
     fi
 
-    echo "$file"
+    # Keep only first line for column names and lines in bounces
+    declare -a limitOptions=()
+    limitOptions+="-v withHeader=1"
+    if [[ ${limitRange} -eq 2 ]]; then
+        limitOptions+="-v rowOffset=${limit[0]}"
+        limitOptions+="-v rowCount=${limit[1]}"
+    else
+        limitOptions+="-v rowCount=${limit[0]}"
+    fi
+    awk ${limitOptions[@]} -f "${AWQL_TERM_TABLES_DIR}/termTable.awk" "$file" > "$wrkFile"
+    if [[ $? -ne 0 ]]; then
+        return 1
+    fi
+
+    echo "$wrkFile"
+}
+
+##
+# Manage order clause
+# @example d 2 0
+# @param string $1 Filepath
+# @param string $2 OrderBy
+# @return string Filepath
+function __sortingRows ()
+{
+    local file="$1"
+    if [[ -z "$file" || ! -f "$file" || "$file" != *"${AWQL_FILE_EXT}" ]]; then
+        return 1
+    fi
+    declare -a orders
+    IFS="," read -a orders <<<"$2"
+    declare -i numberOrders="${#orders[@]}"
+    if [[ 0 -eq ${numberOrders} ]]; then
+        echo "$file"
+        return 0
+    fi
+
+    local wrkFile="${file//${AWQL_FILE_EXT}/__${orders/ /-}${AWQL_FILE_EXT}}"
+    if [[ -f "$wrkFile" ]]; then
+        # Job already done
+        echo "$wrkFile"
+        return 0
+    fi
+
+    # Input field separator
+    declare -a sortOptions=()
+    sortOptions+="-t,"
+
+    local sort=""
+    declare -i pos=0
+    for (( pos=0; pos < ${numberOrders}; pos++ )); do
+        declare -a order="(${orders[${pos}]})"
+        if [[ 3 -ne "${#order[@]}" ]]; then
+            return 1
+        fi
+
+        # Also see syntax: -k+${order[1]} -${order[0]} [-r]
+        sort="-k${order[1]},${order[1]}${order[0]}"
+        if [[ ${AWQL_SORT_ORDER_DESC} -eq ${order[2]} ]]; then
+            sort+="r"
+        fi
+        sortOptions+="$sort"
+    done
+
+    head -1 "$file" > "$wrkFile" && sed 1d "$file" | sort ${sortOptions[@]} >> "$wrkFile"
+    if [[ $? -ne 0 ]]; then
+        return 1
+    fi
+
+    echo "$wrkFile"
 }
 
 ##
@@ -119,10 +239,11 @@ function __printContext ()
 }
 
 ##
-# Print CSV file with ShCsv
+# Print CSV file with termTables
 #
 # @param string $1 File path
 # @param int $2 Vertical mode
+# @param string $3 Headers
 # @return string
 # @returnStatus 1 If file is empty or do not exist
 function __printFile ()
@@ -132,13 +253,19 @@ function __printFile ()
         return 1
     fi
     declare -i vertical="$2"
+    local headers="$3"
 
-    # Format CVS to print it in shell terminal
-    local cvsOptions
+    # Format CVS to display it in a shell terminal
+    declare -a csvOptions=()
     if [[ ${vertical} -eq 1 ]]; then
-        cvsOptions="-g"
+        csvOptions+="-v verticalMode=1"
     fi
-    echo -e "$(${AWQL_CSV_TOOL_FILE} ${cvsOptions} -f "$file")"
+    # Change some columns names
+    if [[ -n "$headers" ]]; then
+        csvOptions+="-v fieldNames=\"${headers}\""
+    fi
+
+    awk ${csvOptions[@]} -f "${AWQL_TERM_TABLES_DIR}/termTable.awk" "$file"
 }
 
 ##
@@ -155,17 +282,36 @@ function awqlResponse ()
     declare -A response="$2"
 
     # Print Awql response
-    declare -i fileSize=0
     local file="${response["${AWQL_RESPONSE_FILE}"]}"
-    file=$(__buildDataFile "$file" "${request["${AWQL_REQUEST_LIMIT}"]}" "${request["${AWQL_REQUEST_SORT_ORDER}"]}")
-    if [[ $? -eq 0 ]]; then
-        # File exists and has one result line at least
-        __printFile "$file" "${request["${AWQL_REQUEST_VERTICAL}"]}"
-        if [[ $? -ne 0 ]]; then
-            echo "${AWQL_INTERNAL_ERROR_DATA_FILE}"
-            return 1
-        fi
-        fileSize=$(wc -l < "$file")
+    declare -i fileSize=$(wc -l < "$file")
+    if [[ ${fileSize} -le 1 ]]; then
+        # No result in file
+        return 2
+    fi
+
+    # Manage group by, avg, distinct, count or sum methods
+    file=$(__aggregateRows "$file" "${request["${AWQL_REQUEST_AGGREGATES}"]}" "${request["${AWQL_REQUEST_GROUP}"]}")
+    if [[ $? -ne 0 ]]; then
+        return 1
+    fi
+
+    # Manage order clause
+    file=$(__sortingRows "$file" "${request["${AWQL_REQUEST_SORT_ORDER}"]}")
+    if [[ $? -ne 0 ]]; then
+        return 1
+    fi
+
+    # Manage limit clause
+    file=$(__limitRows "$file" "${request["${AWQL_REQUEST_LIMIT}"]}")
+    if [[ $? -ne 0 ]]; then
+        return 1
+    fi
+
+    # At least one result line to display
+    __printFile "$file" "${request["${AWQL_REQUEST_VERTICAL}"]}" "${request["${AWQL_REQUEST_HEADERS}"]}"
+    if [[ $? -ne 0 ]]; then
+        echo "${AWQL_INTERNAL_ERROR_DATA_FILE}"
+        return 1
     fi
 
     # Add context (file size, time duration, etc.)
